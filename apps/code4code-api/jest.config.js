114b3df821b395fa4c1170d8d895a2322bb1e086
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-12-19 19:48:51 +0200

    api -> code4code-api

--- apps/code4code-api/jest.config.js
+++ apps/code4code-api/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'api',
+  displayName: 'code4code-api',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/api',
+  coverageDirectory: '../../coverage/apps/code4code-api',
 };
