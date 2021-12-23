f631df1d9b76070756d4b6a1d0fad1b26dbd6fef
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-12-23 11:04:13 +0200

    @ngneat/svg-icons

---
+++ libs/ui/src/svg/arrows/index.ts
@@ -0,0 +1,8 @@
+import { appArrowDownIcon } from './arrow-down';
+import { appArrowLeftIcon } from './arrow-left';
+import { appArrowNarrowDownIcon } from './arrow-narrow-down';
+import { appArrowNarrowLeftIcon } from './arrow-narrow-left';
+import { appArrowNarrowRightIcon } from './arrow-narrow-right';
+import { appArrowNarrowUpIcon } from './arrow-narrow-up';
+import { appArrowRightIcon } from './arrow-right';
+export const arrowsIcons = [appArrowDownIcon, appArrowLeftIcon, appArrowNarrowDownIcon, appArrowNarrowLeftIcon, appArrowNarrowRightIcon, appArrowNarrowUpIcon, appArrowRightIcon];
