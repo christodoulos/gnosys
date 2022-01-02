7a2e5dc8157cdc79f56b23f33f80d0165a3afb40
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-01-02 10:07:03 +0200

    top-login0 component

--- libs/ui/src/lib/top-login0/top-login0.component.ts
+++ libs/ui/src/lib/top-login0/top-login0.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'top-login0',
   templateUrl: './top-login0.component.html',
   styleUrls: ['./top-login0.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TopLogin0Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class TopLogin0Component {}
