a43f0cde92e5dd33ad7beb345368833b217adeb4
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-01-24 11:43:22 +0200

    mapbox marker component

--- libs/mapboxgl/src/index.ts
+++ libs/mapboxgl/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/mapboxgl.module';
 export * from './lib/map/map.component';
 export * from './lib/layer/layer.component';
+export * from './lib/marker/marker.component';
 export * from './lib/map/map.service';
