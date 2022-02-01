de177229c222e981c0fb6d2b777d5148e3c93c5d
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-02-01 18:37:03 +0200

    new form2textbuffercomponent

---
+++ libs/ui/src/lib/form-to-text-buffer/form-to-text-buffer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'gnosys-form-to-text-buffer',
+  templateUrl: './form-to-text-buffer.component.html',
+  styleUrls: ['./form-to-text-buffer.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FormToTextBufferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
